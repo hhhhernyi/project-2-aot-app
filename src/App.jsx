62866fe5cbf2818defffb5737fa7b7cf9af05209
Hern Yi Lee <hyileenet@gmail.com>
2025-01-24 16:15:51 +0800

    fetching data done

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,54 @@
 // src/App.jsx
+import { useState, useEffect } from "react";
+import {getCharacters,getEpisodes,getLocations,getOrganizations,getTitans} from "./services/AOTservice";
 
 const App = () => {
+  const [characters, setCharacters] = useState({})
+  const [episodes, setEpisodes] = useState([])
+  const [locations, setLocations] = useState([])
+  const [organizations, setOrganization] = useState([])
+  const [titans, setTitans] = useState([])
+
+    useEffect(()=>{
+        const getData = async() => {
+            const charactersData = await getCharacters();
+            setCharacters(charactersData)
+        };
+        getData();
+    },[]);
+
+    useEffect(()=>{
+      const getData = async() => {
+          const episodesData = await getEpisodes();
+          setEpisodes(episodesData)
+      };
+      getData();
+  },[]);
+
+  useEffect(()=>{
+    const getData = async() => {
+        const locationsData = await getLocations();
+        setLocations(locationsData)
+    };
+    getData();
+},[]);
+
+useEffect(()=>{
+  const getData = async() => {
+      const organizationData = await getOrganizations();
+      setOrganization(organizationData)
+  };
+  getData();
+},[]);
+
+useEffect(()=>{
+  const getData = async() => {
+      const titansData = await getTitans();
+      setTitans(titansData)
+  };
+  getData();
+},[]);
+
   return <h1>Hello world!</h1>;
 };
 
--- src/services/AOTservice.js
+++ src/services/AOTservice.js
@@ -1,3 +1,81 @@
 // src/services/petService.js
 
 const BASE_URL = `${import.meta.env.VITE_BACK_END_SERVER_URL}`;
+
+async function getCharacters() {
+    const url = BASE_URL + 'characters'
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
+      }
+  
+      const json = await response.json();
+      console.log(json);
+      return json;
+    } catch (error) {
+      console.error(error.message);
+    }
+  }
+  async function getEpisodes() {
+    const url = BASE_URL + 'episodes'
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
+      }
+  
+      const json = await response.json();
+      console.log(json);
+      return json;
+    } catch (error) {
+      console.error(error.message);
+    }
+  }
+  async function getLocations() {
+    const url = BASE_URL + 'locations'
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
+      }
+  
+      const json = await response.json();
+      console.log(json);
+      return json;
+    } catch (error) {
+      console.error(error.message);
+    }
+  }
+  async function getOrganizations() {
+    const url = BASE_URL + 'organizations'
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
+      }
+  
+      const json = await response.json();
+      console.log(json);
+      return json;
+    } catch (error) {
+      console.error(error.message);
+    }
+  }
+  async function getTitans() {
+    const url = BASE_URL + 'titans'
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
+      }
+  
+      const json = await response.json();
+      console.log(json);
+      return json;
+    } catch (error) {
+      console.error(error.message);
+    }
+  }
+
+  export {getCharacters,getEpisodes,getLocations,getOrganizations,getTitans}
