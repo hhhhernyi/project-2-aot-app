6833f0634a925998080f9dbd049bcc43fc2ba0aa
Hern Yi Lee <hyileenet@gmail.com>
2025-01-28 09:52:32 +0800

    changed home page symbols

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,5 @@
-import videoImg from '../assets/images/crunchyrollLogo.png'
-import mangaImg from '../assets/images/AOT_manga_cover.jpeg'
+import videoImg from '../assets/images/video.png'
+import mangaImg from '../assets/images/book.png'
 
 
 export default function HomePage() {
@@ -19,7 +19,13 @@ export default function HomePage() {
                 <p className='synopsisTitle'>Synopsis</p>
                 <p className='synopsisText'>"That day, the human race remembered the terror of being dominated by them, and the shame of being held captive in a birdcage..." — Over 100 years ago, a natural predator of humanity appeared: the Titans, giant humanoid but mindless monsters whose sole purpose of existence seemed to be to devour humans. There was an insurmountable gap in power between them and mankind, and as a result, humanity was rapidly exterminated to the brink of extinction. The survivors responded by constructing three concentric walls: Wall Maria, Wall Rose and Wall Sina, which graced them with a century of peace. However, one day a Colossal Titan far larger than any other seen before breached the outer wall, allowing the smaller Titans to invade the human territory and forcing the survivors to retreat to the inner walls. Eren Jaeger, a boy whose mother was eaten during the invasion, vowed to wipe every last Titan off the face of the Earth, and joined the military determined to exact his revenge.</p>
             </div>
+            <footer>
+                <p>logo attributions</p>
+                <p>Book by Pablo Rozenberg from <a href="https://thenounproject.com/browse/icons/term/book/" target="_blank" title="Book Icons">Noun Project</a> (CC BY 3.0)</p>
+                <p>video streaming by ProSymbols from <a href="https://thenounproject.com/browse/icons/term/video-streaming/" target="_blank" title="video streaming Icons">Noun Project</a> (CC BY 3.0)</p>
+            </footer>
             
         </>
     )
 };
+ 
